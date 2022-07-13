49fd461a3219327c6adb00c27a39ec1201e74e33
farwkwk <109227347+farwkwk@users.noreply.github.com>
2022-07-13 22:27:47 +0700

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -3,11 +3,11 @@
 /******************************************************* SETUP ************************************************************/
 
 const config = {
-    name: "Friend", // actual name of the recipient (Mandatory)
+    name: "Farah", // actual name of the recipient (Mandatory)
 
-    nickname: "Buddy", // nickname(optional)
+    nickname: "farah", // nickname(optional)
 
-    pic: "", //image url of recipients (Mandatory)
+    pic: "https://telegra.ph/buat-farah-07-13", //image url of recipients (Mandatory)
 
     showScrollMsg: true, // set to false if you do not want the scrolling message
 };
