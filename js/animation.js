a34638883d096e47fd8e7013c0e253e2a7b95b5c
farwkwk <109227347+farwkwk@users.noreply.github.com>
2022-07-13 22:47:00 +0700

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -176,7 +176,7 @@ button.addEventListener("click",function(){
             return;
         }
 
-        document.querySelector(".HBD").textContent = "May your soul rest in peace";
+        document.querySelector(".HBD").textContent = "Happy Birthday";
 
         frames[1].style.display = "flex";
 
